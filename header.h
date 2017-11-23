9e38d537f48c66a1784405cbd17179af7f486358
Nicolas VERGNAC <nvergnac@e2r13p12.42.fr>
2017-11-23 21:00:07 +0100

    FILL IT DONE BIATCH

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: nvergnac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 16:45:11 by nvergnac          #+#    #+#             */
-/*   Updated: 2017/11/22 17:50:04 by pclement         ###   ########.fr       */
+/*   Updated: 2017/11/23 17:03:20 by nvergnac         ###   ########.fr       */
 /*   Updated: 2017/11/21 18:14:13 by pclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -21,8 +21,8 @@ typedef struct		s_info
 {
 	int				nb_tetri;
 	int				min_square;
-	int				max_square;
 	char			**tab;
+	char			*free_tetri;
 }					t_info;
 
 typedef struct		s_tetri
@@ -32,8 +32,6 @@ typedef struct		s_tetri
 	struct s_tetri	*next;
 	int				heigth;
 	int				width;
-	int				opt_pos[2];
-	int				test_pos[2];
 }					t_tetri;
 
 
@@ -41,7 +39,7 @@ int					ft_str_check(char *s);
 void				ft_getclean_coord(t_tetri *element);
 t_tetri				*ft_fill_struct(char *buf, char a);
 t_tetri				*ft_treatment(int fd);
-void				ft_solver(t_tetri *lst);
+int				ft_solver(t_tetri *lst);
 int					ft_int_strlen(char *s);
 #endif
 
